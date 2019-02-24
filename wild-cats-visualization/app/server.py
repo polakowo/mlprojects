35bddddb2545a402b0936867a2d4b81a5cc9fed8
Oleg Polakow <olegpolakow@gmail.com>
2019-02-24 13:02:53 +0100

    Show heatmap in the app

--- wild-cats-visualization/app/server.py
+++ wild-cats-visualization/app/server.py
@@ -1,18 +1,22 @@
 from starlette.applications import Starlette
-from starlette.responses import HTMLResponse, JSONResponse
-from starlette.middleware.cors import CORSMiddleware
+from starlette.templating import Jinja2Templates
 import uvicorn
 import aiohttp
 import asyncio
 import sys
+
 from io import BytesIO
 from PIL import Image
-from fastai import vision
+from fastai import vision, callbacks
+from scipy import ndimage
+import cv2
 import base64
+import numpy as np
+import matplotlib.pyplot as plt
 
 path = vision.Path(__file__).parent
-app = Starlette(debug=True, template_directory=path/'templates')
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app = Starlette(debug=True)
+templates = Jinja2Templates(str(path/'templates'))
 
 
 async def setup_learner():
@@ -31,12 +35,55 @@ async def get_bytes(url):
             return await response.read()
 
 
+def hooked_backward(xb):
+    m = learn.model.eval()
+    with callbacks.hook_output(m[0]) as hook_a:
+        with callbacks.hook_output(m[0], grad=True) as hook_g:
+            preds = m(xb)[0]
+            pred = vision.torch.argmax(preds)
+            preds[pred].backward()
+    return hook_a, hook_g, preds
+
+
+def upsample(im, to_shape):
+    heatmap = ndimage.zoom(im, (to_shape[0] / im.shape[0], to_shape[1] / im.shape[1]), order=1)
+    heatmap = (heatmap - np.min(heatmap)) / (np.max(heatmap) - np.min(heatmap))
+    return heatmap
+
+
 async def get_prediction(file):
     bytes = await (file.read())
     img = vision.open_image(BytesIO(bytes))
-    _, _, probs = learn.predict(img)
+
+    # Preprocess the image for prediction
+    xb, _ = learn.data.one_item(img)
+    xb_im = np.moveaxis(learn.data.denorm(xb)[0].numpy(), 0, -1)
+
+    # Retrieve the feature maps and the gradients
+    hook_a, hook_g, preds = hooked_backward(xb)
+    probs = vision.torch.softmax(preds, dim=0)
     label, prob = sorted(zip(learn.data.classes, map(float, probs)), key=lambda p: p[1], reverse=True)[0]
-    image = base64.b64encode(bytes).decode("utf-8")
+    acts = hook_a.stored[0].cpu()
+    grad = hook_g.stored[0][0].cpu()
+
+    # Process the heatmap
+    grad_chan = grad.mean(1).mean(1)
+    heatmap = (acts * grad_chan[..., None, None]).mean(0).numpy()
+    heatmap = upsample(heatmap, xb_im.shape[:2])
+    heatmap = plt.cm.plasma(heatmap)[..., :3]
+
+    # Overlay the image and the heatmap
+    heatmap = heatmap.astype(xb_im.dtype)
+    overlay_img = cv2.addWeighted(xb_im, 0.5, heatmap, 0.5, 0.)
+    overlay_img *= 255
+    overlay_img = overlay_img.astype(np.uint8)
+    overlay_img = Image.fromarray(overlay_img)
+
+    # Prepare the output
+    buffered = BytesIO()
+    overlay_img.save(buffered, format='PNG')
+    image = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
     return {
         'image': image,
         'fname': file.filename,
@@ -51,16 +98,12 @@ async def upload(request):
     predictions = list()
     for file in data.getlist('files'):
         predictions.append(await get_prediction(file))
-    template = app.get_template('predict.html')
-    content = template.render(items=predictions)
-    return HTMLResponse(content)
+    return templates.TemplateResponse("predict.html", {"items": predictions, "request": request})
 
 
 @app.route("/")
 def form(request):
-    template = app.get_template('upload.html')
-    content = template.render()
-    return HTMLResponse(content)
+    return templates.TemplateResponse("upload.html", {"request": request})
 
 
 if __name__ == "__main__":
