2d654fe335463b60ad96824c80c2e9a8a1347dce
Oleg Polakow <olegpolakow@gmail.com>
2019-05-24 01:11:18 +0200

    Automate cards creation and add new projects

---
+++ docs/cards.js
@@ -0,0 +1,230 @@
+cards = [{
+    "img_url": "img/1c-sales-prediction.png",
+    "title": "1C COMPANY SALES PREDICTION",
+    "description": "Predict total sales for every product and store in the next month",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/1c-sales-prediction",
+    "section": "Business",
+    "category": "projects"
+}, {
+    "img_url": "img/rossmann-sales-prediction.png",
+    "title": "ROSSMANN SALES PREDICTION",
+    "description": "Forecast daily Rossmann sales for up to six weeks in advance",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/rossmann-sales-prediction",
+    "section": "Business",
+    "category": "projects"
+}, {
+    "img_url": "img/credit-card-fraud-detection.jpg",
+    "title": "CREDIT CARD FRAUD DETECTION",
+    "description": "Recognize fraudulent credit card transactions with anomaly detection",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/credit-card-fraud-detection",
+    "section": "Business",
+    "category": "projects"
+}, {
+    "img_url": "img/movielens-recommendation.jpg",
+    "title": "MOVIE RECOMMENDER SYSTEM",
+    "description": "Build a movie recommender system using item-based collaborative filtering",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/movielens-recommendation",
+    "section": "Business",
+    "category": "projects"
+}, {
+    "img_url": "img/airbus-ship-segmentation.png",
+    "title": "AIRBUS SHIP SEGMENTATION",
+    "description": "Build a model that detects ships in satellite images",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/airbus-ship-segmentation",
+    "section": "Computer Vision",
+    "category": "projects"
+}, {
+    "img_url": "img/planet-amazon-classification.jpg",
+    "title": "AMAZON FROM SPACE CLASSIFICATION",
+    "description": "Label satellite image chips with atmospheric conditions and various classes of land cover/land use",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/planet-amazon-classification",
+    "section": "Computer Vision",
+    "category": "projects"
+}, {
+    "img_url": "img/amazon-reviews-sentiment-analysis.jpg",
+    "title": "AMAZON REVIEWS SENTIMENT ANALYSIS",
+    "description": "Analyze Amazon reviews to predict the sentiments that the reviews express",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/amazon-reviews-sentiment-analysis",
+    "section": "Natural Language Processing",
+    "category": "projects"
+}, {
+    "img_url": "img/optimized-transfer-learning.jpg",
+    "title": "OPTIMIZED TRANSFER LEARNING",
+    "description": "Use an optimized approach to fine-tune Keras models faster",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/optimized-transfer-learning",
+    "section": "Research",
+    "category": "projects"
+}, {
+    "img_url": "img/visual-model-explanation.png",
+    "title": "VISUAL MODEL EXPLANATION",
+    "description": "Explain the workings of convolutional networks by using automated interpretation methods",
+    "url": "https://github.com/polakowo/machine-learning/tree/master/projects/visual-model-explanation",
+    "section": "Research",
+    "category": "projects"
+}, {
+    "img_url": "img/deep-learning.png",
+    "title": "FIELD OF DEEP LEARNING",
+    "description": "An introduction to deep artificial neural networks and deep learning",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Deep-Learning.ipynb",
+    "section": "General",
+    "category": "notes"
+}, {
+    "img_url": "img/ml-strategy.jpg",
+    "title": "MACHINE LEARNING STRATEGY",
+    "description": "Tips on how to build a successful machine learning project",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/ML-Strategy.ipynb",
+    "section": "General",
+    "category": "notes"
+}, {
+    "img_url": "img/backpropagation.png",
+    "title": "BACKPROPAGATION",
+    "description": "Backpropagation and gradient checking for efficiently training neural networks",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Backpropagation.ipynb",
+    "section": "Fundamentals",
+    "category": "notes"
+}, {
+    "img_url": "img/activation-functions.png",
+    "title": "ACTIVATION FUNCTIONS",
+    "description": "Commonly used activation functions for diverse architectures",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Activation-Functions.ipynb",
+    "section": "Fundamentals",
+    "category": "notes"
+}, {
+    "img_url": "img/initialization.png",
+    "title": "WEIGHTS INITIALIZATION",
+    "description": "Dangers of bad weight initialization and the best-practice methods to use",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Initialization.ipynb",
+    "section": "Fundamentals",
+    "category": "notes"
+}, {
+    "img_url": "img/optimization.png",
+    "title": "OPTIMIZATION",
+    "description": "Types of optimization algorithms used in neural networks and ways to optimize gradient descent",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Optimization.ipynb",
+    "section": "Fundamentals",
+    "category": "notes"
+}, {
+    "img_url": "img/regularization.png",
+    "title": "REGULARIZATION",
+    "description": "Regularization and data augmentation as ways to adjust bias and variance",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Regularization.ipynb",
+    "section": "Fundamentals",
+    "category": "notes"
+}, {
+    "img_url": "img/convnets.png",
+    "title": "CONVOLUTIONAL NEURAL NETWORKS",
+    "description": "Fundamentals and main building blocks of convolutional neural networks",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/ConvNets.ipynb",
+    "section": "Computer Vision",
+    "category": "notes"
+}, {
+    "img_url": "img/convnet-architectures.png",
+    "title": "ARCHITECTURES",
+    "description": "Evolution and comparison of commonly used architectures for convolutional networks",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/ConvNet-Architectures.ipynb",
+    "section": "Computer Vision",
+    "category": "notes"
+}, {
+    "img_url": "img/object-detection.png",
+    "title": "OBJECT DETECTION",
+    "description": "Key concepts in object detection explained with the help of YOLO",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Object-Detection.ipynb",
+    "section": "Computer Vision",
+    "category": "notes"
+}, {
+    "img_url": "img/face-recognition.jpg",
+    "title": "FACE RECOGNITION",
+    "description": "Face recognition and one-shot learning with Siamese networks",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Face-Recognition.ipynb",
+    "section": "Computer Vision",
+    "category": "notes"
+}, {
+    "img_url": "img/neural-style-transfer.png",
+    "title": "NEURAL STYLE TRANSFER",
+    "description": "Logic and maths behind remarkable style transfer effects",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Neural-Style-Transfer.ipynb",
+    "section": "Computer Vision",
+    "category": "notes"
+}, {
+    "img_url": "img/rnns.jpg",
+    "title": "RECURRENT NEURAL NETWORKS",
+    "description": "Fundamentals and main building blocks of recurrent neural networks",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/RNNs.ipynb",
+    "section": "Natural Language Processing",
+    "category": "notes"
+}, {
+    "img_url": "img/word-embeddings.png",
+    "title": "WORD EMBEDDINGS",
+    "description": "A set of basic modeling and feature learning techniques for NLP",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Word-Embeddings.ipynb",
+    "section": "Natural Language Processing",
+    "category": "notes"
+}, {
+    "img_url": "img/nmt.png",
+    "title": "NEURAL MACHINE TRANSLATION",
+    "description": "Text translation with deep learning and sequence models",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/NMT.ipynb",
+    "section": "Natural Language Processing",
+    "category": "notes"
+}, {
+    "img_url": "img/attention-mechanism.jpg",
+    "title": "ATTENTION MECHANISM",
+    "description": "A brief overview of attention mechanism for learning long-range dependencies",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Attention-Mechanism.ipynb",
+    "section": "Natural Language Processing",
+    "category": "notes"
+}, {
+    "img_url": "img/speech-recognition.png",
+    "title": "SPEECH RECOGNITION",
+    "description": "A common approach of speech recognition using spectrograms",
+    "url": "https://nbviewer.jupyter.org/github/polakowo/machine-learning/blob/master/notes/Speech-Recognition.ipynb",
+    "section": "Natural Language Processing",
+    "category": "notes"
+}]
+
+const generate_section = (name) => $(`
+    <div class="row">
+        <div class="col">
+            <hr>
+        </div>
+        <div class="col-auto">${name}</div>
+        <div class="col">
+            <hr>
+        </div>
+    </div>
+`)
+const generate_row = () => $('<div class="row"></div>')
+const generate_card = (item) => $(`
+    <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+            <div class="img-container">
+                <img class="card-img-top" src="${item.img_url}">
+                <div class="text-overlay">
+                    <span>${item.title}</span>
+                </div>
+            </div>
+            <div class="card-body">
+                <p class="card-text">${item.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <a href="${item.url}"
+                            target="_blank" role="button"
+                            class="btn btn-sm btn-outline-secondary">View</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`)
+
+sections = []
+cards.forEach((item, i) => {
+    section_id = `${item.category}-${item.section}`
+    $container = $(`#${item.category}-container`)
+    if (!sections.includes(section_id)) {
+        sections.push(section_id)
+        $container.append(generate_section(item.section))
+        $container.append(generate_row())
+    }
+    $container.find('div.row:last').append(generate_card(item))
+})
