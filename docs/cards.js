50d7f5519a27aca5122603937560189a850293d1
Oleg Polakow <olegpolakow@gmail.com>
2019-06-02 21:49:13 +0200

    Update image

--- docs/cards.js
+++ docs/cards.js
@@ -31,7 +31,7 @@ cards = [{
     "section": "Business",
     "category": "projects"
 }, {
-    "img_url": "img/credit-card-fraud-detection.jpg",
+    "img_url": "img/credit-card-fraud-detection.png",
     "title": "CREDIT CARD FRAUD DETECTION",
     "description": "Recognize fraudulent credit card transactions with anomaly detection",
     "tags": ['sklearn', 'PyTorch', 'fastai'],
